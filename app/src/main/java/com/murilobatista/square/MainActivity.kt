5a5c9ea431e43224dcfcda2da7a50a60579b8b83
muriloconceicao <daconceicaomurilo@gmail.com>
2020-02-19 01:14:25 -0300

    Main function of project is done

---
+++ app/src/main/java/com/murilobatista/square/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.murilobatista.square
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        db = FirebaseFirestore.getInstance()
+        btnMudar.setOnClickListener { mudarQuadrado() }
+        db.collection("formas").get().addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d("Formas", "${document.id} => ${document.data}")
+            }
+        }
+    }
+
+    private fun mudarQuadrado() {
+        val largura = editTextLargura.text.toString()
+        val altura = editTextAltura.text.toString()
+        if(largura.isNotEmpty() && altura.isNotEmpty()) {
+            progressBar.visibility = View.VISIBLE
+            val quadrado = hashMapOf("width" to largura, "height" to altura)
+            db.collection("formas").document("quadrado").update(quadrado as Map<String, Any>)
+                .addOnSuccessListener { progressBar.visibility = View.GONE; Toast.makeText(this, "Quadrado alterado!", Toast.LENGTH_SHORT).show()}
+        } else {
+            Toast.makeText(this, "Preencha os campos!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
